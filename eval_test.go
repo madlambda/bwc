ec05b8ddddcdd98be4be4bac901d2101df7e37f9
i4k <tiago4orion@gmail.com>
2018-03-26 22:09:19 -0300

    add some eval tests
    Signed-off-by: i4k <tiago4orion@gmail.com>

---
+++ eval_test.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+var format = fmt.Sprintf
+
+func desc(a Expr, res Int) string {
+	return format("%s %s %s = %s",
+		a.Lhs, a.Rhs, a.Op, res)
+}
+
+func TestEval(t *testing.T) {
+	for _, tc := range []struct {
+		expr Expr
+		res  Int
+	}{
+		{
+			expr: Expr{
+				Op:  OpAND,
+				Lhs: Int(0),
+				Rhs: Int(0),
+			},
+			res: 0,
+		},
+		{
+			expr: Expr{
+				Op:  OpAND,
+				Lhs: Int(0),
+				Rhs: Int(1),
+			},
+			res: 0,
+		},
+		{
+			expr: Expr{
+				Op:  OpAND,
+				Lhs: Int(1),
+				Rhs: Int(1),
+			},
+			res: 1,
+		},
+		{
+			expr: Expr{
+				Op:  OpAND,
+				Lhs: Int(0xffff0000),
+				Rhs: Int(0x0000ffff),
+			},
+			res: 0,
+		},
+		{
+			expr: Expr{
+				Op:  OpOR,
+				Lhs: Int(0xffff0000),
+				Rhs: Int(0x0000ffff),
+			},
+			res: 0xffffffff,
+		},
+
+		// groups
+		{
+			expr: Expr{
+				Op: OpAND,
+				Lhs: Expr{
+					Op:  OpOR,
+					Lhs: Int(0xffff0000),
+					Rhs: Int(0x0000ffff),
+				},
+				Rhs: Int(0x000000ff),
+			},
+			res: 0x000000ff,
+		},
+		{
+			expr: Expr{
+				Op: OpOR,
+				Lhs: Expr{
+					Op:  OpOR,
+					Lhs: Int(0x000000ff),
+					Rhs: Int(0xff000000),
+				},
+				Rhs: Expr{
+					Op:  OpOR,
+					Lhs: Int(0x00ff0000),
+					Rhs: Int(0x0000ff00),
+				},
+			},
+			res: 0xffffffff,
+		},
+	} {
+		tc := tc
+		t.Run(desc(tc.expr, tc.res), func(t *testing.T) {
+			got := Eval(tc.expr)
+			if got != tc.res {
+				t.Fatalf("Fail: %d != %d", got, tc.res)
+			}
+		})
+	}
+}
--- main.go
+++ main.go
@@ -2,46 +2,63 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 type (
+	Int int64
 	Expr struct {
-		Op  int
+		Op  Optype
 		Lhs Node
 		Rhs Node
 	}
 
 	Node interface {
-		Type() int
+		Type() Nodetype
+		String() string
 	}
 
-	Int int64
+	Nodetype int
+	Optype int
 )
 
 const (
-	Nodetype = iota
-	NodeExpr
+	NodeExpr Nodetype = iota+1
 	NodeInt
 
-	Optype = iota
-	OpAND
+	OpAND Optype = iota+1
 	OpOR
 )
 
-func (_ Int) Type() int { return NodeInt }
-func (_ Expr) Type() int { return NodeExpr }
+func (o Optype) String() string {
+	if o == OpAND 	{ return "&" }
+	if o == OpOR 	{ return "|" }
+	return "<invalid op>"
+}
 
-func eval(tree Expr) Int {
+func (_ Int) Type() Nodetype  { return NodeInt }
+func (i Int) String() string { return strconv.Itoa(int(i)) }
+
+func (_ Expr) Type() Nodetype { return NodeExpr }
+func (a Expr) String() string {
+	lhs := a.Lhs.String()
+	rhs := a.Rhs.String()
+	op := a.Op.String()
+
+	return fmt.Sprintf("%s %s %s", lhs, rhs, op)
+}
+
+func Eval(tree Expr) Int {
 	var lhs, rhs Int
 
 	if tree.Lhs.Type() != NodeInt {
-		lhs = eval(tree.Lhs.(Expr))
+		lhs = Eval(tree.Lhs.(Expr))
 	} else {
 		lhs = tree.Lhs.(Int)
 	}
 
 	if tree.Rhs.Type() != NodeInt {
-		rhs = eval(tree.Rhs.(Expr))
+		rhs = Eval(tree.Rhs.(Expr))
 	} else {
 		rhs = tree.Rhs.(Int)
 	}
@@ -63,5 +80,5 @@ func main() {
 		Rhs: Int(2),
 	}
 
-	fmt.Printf("%d\n", eval(expr))
+	fmt.Printf("%d\n", Eval(expr))
 }
